e65212fefafeecb41d5e0538c7f59053a65d96a1
Andres r hernandez <frodriguezh@unal.edu.co>
2021-05-04 16:44:44 -0500

    añadiendo logica al programa, tratando de implementar la entrada con tecla enter

---
+++ app.js
@@ -0,0 +1,44 @@
+(function () {
+  const numbers = document.querySelectorAll('.btn')
+  const signals = document.querySelectorAll('btn-blue')
+  const equal = document.querySelector('.btn-equal')
+  const clear = document.querySelector('.btn-clear')
+  let screen = document.querySelector('.screen')
+
+  numbers.forEach( (number) => {
+    number.addEventListener('click', getNumber = (nmber) => {
+    let newNumber = nmber.target.dataset.num
+    if (screen.value === 'Please enter a value') {
+      screen.value = newNumber
+    } else {
+    screen.value += newNumber}
+  })
+
+    screen.addEventListener('keydown', (e) => {
+      if(e.key === 'Enter') {
+        if(screen.value === '') {
+          screen.value = 'Please enter a value'
+        } else {
+          let output = eval(screen.value)
+          screen.value = output;
+        }
+      }
+    })
+
+    equal.addEventListener('click', calculateTotal = () => {
+      
+    if(screen.value === '') {
+      screen.value = 'Please enter a value'
+    } else {
+      let output = eval(screen.value)
+      screen.value = output;
+    }
+  } ) 
+
+})
+
+  clear.addEventListener('click',clearScreen => {
+    screen.value = ''
+  })
+
+})()
